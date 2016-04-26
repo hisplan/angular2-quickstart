e76f024be2f8f82c2990a2f6f55a9a92dd5f01a8
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2016-04-26 06:30:23 -0400

    Added chart

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Test 1</h1>'
+    template: '<h1>domaninhold_RON</h1>'
 })
 export class AppComponent { }
